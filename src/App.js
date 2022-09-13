24ad5fd14c6fea516ab3cc062c2b4b6c7625353f
Alexey Lozin <kanulilewa@mail.ru>
2022-09-12 19:37:05 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <h1>Ford Mustang GT</h1>
+      <h3>model years: 2005-2009; chassis' code: s197</h3>
+      <ol>
+        <li>two-door coupe - strong resemblance to the original mustang of the 60's</li>
+        <li>5 speed manual transmission - my introduction into the world of stick shifts</li>
+        <li>300hp - a little more than my previous bimmer - but still an upgrade</li>
+        <li>huge potential for upgrades - might become a very personalized project</li>
+      </ol>
+      <img src='/s197 gt.jpg' />
+      <p>more info in this <a href='https://en.wikipedia.org/wiki/Ford_Mustang_(fifth_generation)'>Wikipedia article</a></p>
     </div>
   );
 }
